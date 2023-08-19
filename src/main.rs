49ec4d9898b989eee9d0eee0d3761b2b167f1872
evref <isak.lefevre@hotmail.se>
2023-08-19 06:15:14 +0200

    add game loop skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use flowcash::*;
 
 fn main() {
-    println!("Hello, world!");
+    let mut game_state = initial_game_state();
+
+    // Game Loop
+    loop {
+        let input_state = capture_input_state();
+
+        player_control_system(&mut game_state, &input_state)
+        ai_behavior_system(&mut game_state);
+
+        render_system(&mut game);
+        audio_system(&mut game);
+
+        // wait for vsync?
+    }
 }
